b7e6ead17622cf5fcb9575acc1f6475f314559c6
alicavusoglu <alicavusoglu07@hotmail.com>
2014-09-12 10:14:56 +0300

    WCF Self Hosting

---
+++ WCFSelfHosting/WCFSelfHost/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace WCFSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(WCFService.Service1));
+            host.Open();
+            Console.Write("Servis Başladı. Kapatmak için bir tuşa basın.");
+            Console.Read();
+            host.Close();
+        }
+    }
+}
